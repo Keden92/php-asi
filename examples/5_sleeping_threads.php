d3b7cc6b46eaa0abccede04eba0788ee0c5d34a7
Keden92 <36050854+Keden92@users.noreply.github.com>
2024-07-21 13:18:38 +0200

    Create 5_sleeping_threads.php

---
+++ examples/5_sleeping_threads.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once("../asi.class.php");
+
+$loops = 5;
+$childs = array();
+
+/* Print DateTime */
+asi\async::dts();
+
+for ($i = 0; $i < $loops; $i++)
+{
+	// Create new child-thread
+	$child[$i] = asi\async::new("Example Thread: ".$i);
+	
+	// Disable autowait
+	asi\async::autowait($child[$i], false);
+	
+	// Execute php's sleep() on the child-thread (sleep 5 seconds)
+	// and assing it to the childs variable "example"
+	$child[$i]->example = asi\sleep(5);
+}
+
+/* Print DateTime */
+asi\async::dts();
+
+for ($i = 0; $i < $loops; $i++)
+{
+	// Wait for the child-thread to finish execution on the "example" child variable
+	asi\async::wait($child[$i]->example);
+	
+	// Print the value returnd by child-thread
+	var_dump($child[$i]->example);
+	echo "<br/>";
+	
+	// shutdown the child-thread
+	unset($child[$i]);
+}
+
+/* Print DateTime */
+asi\async::dts();
+
+?>
