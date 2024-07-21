e366691865e996a0d72c9b3a453f9b1b7bbd49d2
Keden92 <36050854+Keden92@users.noreply.github.com>
2024-07-21 14:26:50 +0200

    Create 5_sleeping_threads_Exceptions.php

---
+++ examples/5_sleeping_threads_Exceptions.php
@@ -0,0 +1,64 @@
+<?php
+
+include_once("../asi.class.php");
+include_once("example.helper.php");
+
+$loops = 5;
+$childs = array();
+
+/* Print DateTime */
+asi\async::dts();
+
+for ($i = 0; $i < $loops; $i++)
+{
+	// Create new child-thread and include additional code-files (use full system paths!)
+	$child[$i] = asi\async::new("Example Thread: ".$i, [__DIR__."/example.helper.php"]);
+	
+	// Create Custom Class (defined in example.helper.php)
+	$myclass = new test_class(5);
+	
+	// Pass Class to child Process
+	$child[$i]->myclass = $myclass;
+	
+	// Or directly create the object on the child-thread
+	// $child[$i]->myclass = new asi\test_class(5);
+	
+	// Disable autowait
+	asi\async::autowait($child[$i], false);
+	
+	// Execute the "exception_example()" function on $myclass on the child-thread
+	// !!IMPORTANT both, the main and the child must know the class!!
+	// and assing it to the childs variable "example"
+	try
+	{
+		$child[$i]->example = $child[$i]->myclass->exception_example();
+	}
+	catch (Exception $e)
+	{
+		var_dump($e->getMessage());
+		echo "<br/>";
+	}
+	
+	unset($myclass); // Not needet any more after passing to child
+}
+
+/* Print DateTime */
+asi\async::dts();
+
+for ($i = 0; $i < $loops; $i++)
+{
+	// Wait for the child-thread to finish execution on the "example" child variable
+	asi\async::wait($child[$i]->example);
+	
+	// Print the value returnd by child-thread
+	var_dump($child[$i]->example);
+	echo "<br/>";
+	
+	// shutdown the child-thread
+	unset($child[$i]);
+}
+
+/* Print DateTime */
+asi\async::dts();
+
+?>
